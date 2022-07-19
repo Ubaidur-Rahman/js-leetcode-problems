c3f6fc25b7d871743e89a7dccc4cbf949ea7f1df
Ubaidur-Rahman <ubaidurrahmanmcc@gmail.com>
2022-07-20 03:21:41 +0600

    find median sorted array solved

--- index.js
+++ index.js
@@ -115,9 +115,50 @@ console.log(mergeTwoLists([1, 2, 4], [1, 3, 4]));
 // A word is a maximal substring consisting of non-space characters only.
 
 var lengthOfLastWord = function (s) {
-  const arr = s.trim().split(' ')
+  const arr = s.trim().split(' ');
   return arr[arr.length - 1].length;
 };
 console.log(lengthOfLastWord('        fgdfgdfg fgdfgdfg     '));
+
+// You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+// Increment the large integer by one and return the resulting array of digits.
+
+var plusOne = function (digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i] = digits[i] + 1;
+      return digits;
+    } else {
+      digits[i] = 0;
+    }
+  }
+  digits.unshift(1);
+  return digits;
+};
+
+// console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 9]));
+
+// Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+// The overall run time complexity should be O(log (m+n)).
+
+var findMedianSortedArrays = function (nums1, nums2) {
+  let arr = [...nums1, ...nums2].sort((a, b) => a - b);
+  console.log(arr);
+  if (arr.length % 2 == 0) {
+    console.log((arr[(arr.length / 2)-1]))
+    if ((arr[(arr.length / 2)-1]) == 0 && arr[arr.length / 2] == 0) {
+      return 0;
+    } else {
+      return (arr[(arr.length / 2)-1] + arr[arr.length / 2]) / 2;
+    }
+  } else {
+    return arr[(arr.length - 1) / 2];
+  }
+};
+
+console.log(findMedianSortedArrays([2,2,4,4]
+,  [2,2,4,4]));
 
 
